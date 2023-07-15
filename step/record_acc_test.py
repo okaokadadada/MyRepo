cf923e8ccf7d71c0a60b108c4f1a583fadd8f073
Okada Kousei <104471841+okaokadadada@users.noreply.github.com>
2023-07-15 23:15:50 +0900

    Update record_acc_test.py

--- step/record_acc_test.py
+++ step/record_acc_test.py
@@ -14,7 +14,5 @@ ret = i2c.write_byte_data(address, 0x31, 0x0b)
 while True:
     
     #データ読み込み
-    xl = i2c.read_byte_data(address, 0x00)
-    print(f"xl = {xl}") # 0xE5
     xh = i2c.read_byte_data(address, 0x32)
     print(f"xh = {xh}")
