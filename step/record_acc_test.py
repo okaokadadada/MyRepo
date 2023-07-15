38ae4363b9ca6035adfddce4fedf55d009fd26f4
Okada Kousei <104471841+okaokadadada@users.noreply.github.com>
2023-07-15 23:09:09 +0900

    Create record_acc_test.py

---
+++ step/record_acc_test.py
@@ -0,0 +1,20 @@
+import smbus
+import time
+
+#I2C設定
+i2c = smbus.SMBus(1)
+address = 0x53
+
+#センサーの設定
+# ret = i2c.write_byte_data(address, 0x2C, 0x0B)
+# ret = i2c.write_byte_data(address, 0x31, 0x09)
+ret = i2c.write_byte_data(address, 0x2D, 0x08)
+ret = i2c.write_byte_data(address, 0x31, 0x0b)
+
+while True:
+    
+    #データ読み込み
+    xl = i2c.read_byte_data(address, 0x00)
+    print(f"xl = {xl}") # 0xE5
+    xh = i2c.read_byte_data(address, 0x32)
+    print(f"xh = {xh}")
