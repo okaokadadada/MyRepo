03fc035c71d16792d476b2551bb1a6787832669c
Okada Kousei <104471841+okaokadadada@users.noreply.github.com>
2023-07-25 22:41:25 +0900

    Update code.py

--- step/examples/CircuitPython 8.x/code.py
+++ step/examples/CircuitPython 8.x/code.py
@@ -4,6 +4,7 @@
 import time
 import board
 import adafruit_adxl37x
+import numpy as np
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
@@ -15,6 +16,7 @@ while True:
     x = x-3
     y = y+5.8 
     z = z+6
-    print("x={:.4f},y={:.4f},z={:.4f}".format(x, y, z))
+    norm = np.sqrt(x*x + y*y + z*z)
+    print("norm={:.4f},x={:.4f},y={:.4f},z={:.4f}".format(norm, x, y, z))
     # print("x=%f, y=%f,z=%f m/s^2" % accelerometer.acceleration)
     time.sleep(0.2)
