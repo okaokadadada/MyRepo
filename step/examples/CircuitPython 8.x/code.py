0673703ba6b6c36114fbbac2159debca013ed68f
Okada Kousei <104471841+okaokadadada@users.noreply.github.com>
2023-07-25 22:59:49 +0900

    Update code.py

--- step/examples/CircuitPython 8.x/code.py
+++ step/examples/CircuitPython 8.x/code.py
@@ -5,10 +5,16 @@ import time
 import board
 import adafruit_adxl37x
 import numpy as np
+import datetime
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 # i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 accelerometer = adafruit_adxl37x.ADXL375(i2c)
+
+folder_name = "record"
+jp_time = datetime.datetime.utcnow() + datetime.timedelta(hours=DIFF_JST_FROM_UTC)
+file_name = folder_name + str(jp_time).replace(' ', '_').replace(':', '-').replace('.', '_') + '.csv'
+
 
 while True:
     # print(accelerometer.acceleration)
@@ -19,4 +25,9 @@ while True:
     norm = np.sqrt(x*x + y*y + z*z)
     print("norm={:.4f},x={:.4f},y={:.4f},z={:.4f}".format(norm, x, y, z))
     # print("x=%f, y=%f,z=%f m/s^2" % accelerometer.acceleration)
+    data_csv = [norm, x, y, z]
+    with open(file_name, 'a') as f:
+        writer = csv.writer(f)
+        writer.writerow(data_csv)
+    
     time.sleep(0.2)
